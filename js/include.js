e349d66cfed4e3c6ef3d29d939ceda2b5f8cd036
Frazer <frazergrant345@gmail.com>
2025-11-01 15:47:00 +0000

    update

---
+++ js/include.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const includes = document.querySelectorAll("[data-include]");
+
+    includes.forEach(el => {
+        const file = el.getAttribute("data-include");
+        fetch(file)
+        .then(response => {
+            if (!response.ok) throw new Error(`Error loading ${file}`);
+            return response.text();
+        })
+        .then(data => (el.innerHTML = data))
+        .catch(err => (el.innerHTML = `<p style="color:red;">${err}</p>`));
+    });
+});
